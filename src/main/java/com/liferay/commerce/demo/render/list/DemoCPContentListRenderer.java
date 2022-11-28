c0b8790a69e5332ef8f36b395e602e243b760db4
Jeff Handa <jeffrey.handa@liferay.com>
2022-11-28 17:18:41 -0500

    Product List Renderer Barebones

--- src/main/java/com/liferay/commerce/demo/render/list/DemoCPContentListRenderer.java
+++ src/main/java/com/liferay/commerce/demo/render/list/DemoCPContentListRenderer.java
@@ -1,7 +1,65 @@
 package com.liferay.commerce.demo.render.list;
 
+import com.liferay.commerce.product.constants.CPPortletKeys;
+import com.liferay.commerce.product.content.render.list.CPContentListRenderer;
+import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 /**
  * @author Jeff Handa
  */
-public class DemoCPContentListRenderer {
+@Component(
+        property = {
+                "commerce.product.content.list.renderer.key=" + DemoCPContentListRenderer.KEY,
+                "commerce.product.content.list.renderer.order=" + Integer.MIN_VALUE + 10,
+                "commerce.product.content.list.renderer.portlet.name=" + CPPortletKeys.CP_PUBLISHER_WEB,
+                "commerce.product.content.list.renderer.portlet.name=" + CPPortletKeys.CP_SEARCH_RESULTS,
+                "commerce.product.content.list.renderer.portlet.name=" + CPPortletKeys.CP_COMPARE_CONTENT_WEB,
+        },
+        service = CPContentListRenderer.class
+)
+public class DemoCPContentListRenderer implements CPContentListRenderer {
+
+        public static final String KEY = "demo-list-renderer";
+
+        @Override
+        public String getKey() {
+                return KEY;
+        }
+
+        @Override
+        public String getLabel(Locale locale) {
+                ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+                        "content.Language", locale, getClass());
+
+                return LanguageUtil.get(resourceBundle, "demo-list-renderer");
+        }
+
+        @Override
+        public void render(
+                HttpServletRequest httpServletRequest,
+                HttpServletResponse httpServletResponse)
+                throws Exception {
+
+                _jspRenderer.renderJSP(
+                        _servletContext, httpServletRequest, httpServletResponse,
+                        "/list_render/view.jsp");
+        }
+
+        @Reference(
+                target = "(osgi.web.symbolicname=com.liferay.commerce.demo.render)"
+        )
+        private ServletContext _servletContext;
+
+        @Reference
+        private JSPRenderer _jspRenderer;
 }
--- src/main/resources/META-INF/resources/list_render/view.jsp
+++ src/main/resources/META-INF/resources/list_render/view.jsp
@@ -1,2 +1,47 @@
 <%@ include file="/META-INF/resources/init.jsp" %>
 
+<%
+    CPContentHelper cpContentHelper = (CPContentHelper) request.getAttribute(CPContentWebKeys.CP_CONTENT_HELPER);
+    CPDataSourceResult cpDataSourceResult = (CPDataSourceResult) request.getAttribute(CPWebKeys.CP_DATA_SOURCE_RESULT);
+    CommerceContext commerceContext = (CommerceContext) request.getAttribute(CommerceWebKeys.COMMERCE_CONTEXT);
+%>
+
+<c:choose>
+    <c:when test="<%= !cpDataSourceResult.getCPCatalogEntries().isEmpty() %>">
+        <div class="product-card-tiles">
+            <%
+                for (CPCatalogEntry cpCatalogEntry : cpDataSourceResult.getCPCatalogEntries()) {
+                    CPSku cpSku = cpContentHelper.getDefaultCPSku(cpCatalogEntry);
+                    String addToCartId = PortalUtil.generateRandomKey(request, "add-to-cart");
+                    String productDetailURL = cpContentHelper.getFriendlyURL(cpCatalogEntry, themeDisplay);
+            %>
+            <div class="cp-renderer">
+                <div class="card d-flex flex-column product-card">
+                    <div class="card-item-first position-relative">
+                        <div class="card-body d-flex flex-column justify-content-between py-2">
+                            <a href="<%= productDetailURL %>">
+                                <liferay-adaptive-media:img
+                                    class="img-fluid product-card-picture"
+                                    fileVersion="<%= cpContentHelper.getCPDefinitionImageFileVersion(cpCatalogEntry.getCPDefinitionId(), request) %>"
+                                />
+                            </a>
+                            <div class="cp-information">
+                                <a href="<%= productDetailURL %>">
+                                    <h1><%= HtmlUtil.escape(cpCatalogEntry.getName()) %></h1>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <%
+                }
+            %>
+        </div>
+    </c:when>
+    <c:otherwise>
+        <div class="alert alert-info">
+            <liferay-ui:message key="no-products-were-found"/>
+        </div>
+    </c:otherwise>
+</c:choose>
